ec8ca74099cb207f2b68624f7864d81f53763755
Heathcliff <heathcliff@heathcliff.eu>
2025-05-11 20:59:51 +0200

    linux: Run dbus call in goroutine
    It seems even the file chooser dbus call is blocking. Ensure the main thread
    keeps running by doing the whole dialog in a goroutine.
    
    Fixes: #12
    
    Add id to testapp to get rid of error message.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- dialog_linux_test.go
+++ dialog_linux_test.go
@@ -4,6 +4,7 @@ package godialog
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -41,4 +42,39 @@ func TestConvertFiltersToFreedesktopFilter(t *testing.T) {
 	converted := convertFiltersToFreedesktopFilter(filter)
 
 	assert.Equal(t, expected, converted, "Should convert filters correctly")
+}
+
+func TestDialogDoesNotBlock(t *testing.T) {
+	fd := NewFileDialog()
+
+	t.Run("Open", func(t *testing.T) {
+		done := make(chan struct{})
+
+		go func() {
+			fd.Open("Test Open", func(path string, err error) {})
+			close(done)
+		}()
+
+		select {
+		case <-done:
+			// Test passed
+		case <-time.After(1 * time.Second):
+			t.Error("Dialog did not return in time")
+		}
+	})
+	t.Run("Save", func(t *testing.T) {
+		done := make(chan struct{})
+
+		go func() {
+			fd.Save("Test Open", func(path string, err error) {})
+			close(done)
+		}()
+
+		select {
+		case <-done:
+			// Test passed
+		case <-time.After(1 * time.Second):
+			t.Error("Dialog did not return in time")
+		}
+	})
 }
